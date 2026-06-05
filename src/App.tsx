8aa852094f3a565ab18c3352dd714b56c6218ae6
syedm121 <syedm333222@gmail.com>
2026-06-05 02:18:47 -0400

    Initial starter setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+export default function App() {
+ 
+
+  return (
+    <div className="min-h-screen bg-background text-foreground flex items-center justify-center p-8">
+   
+    </div>
+  );
+}
